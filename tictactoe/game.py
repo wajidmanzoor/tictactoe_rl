ff3956167ede83b3b3563f31f99fa3fedd6f2ffc
wajidmanzoor <wajid.manzoor.1999@gmail.com>
2021-07-31 20:23:25 +0530

    check result added

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,6 +1,7 @@
 import numpy as np
-board = -1*np.ones((3,3),dtype='int32')
+
 weights = np.zeros((9,9))
+selection_times = np.zeros((9,9))
 """board[0][0] = 1
 board[2][2] =0"""
 choice_board = np.array([(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)])
@@ -25,18 +26,60 @@ def print_(board):
 
 def choose(board,pos,sign):
     board[pos[0]][pos[1]]=sign
+    
     return board
 
-def play(board,weights,choice_board,index):
+def update_weights(weights,board,sign):
+    pass 
+
+
+
+
+
+def check_result(board,sign):
+    if board[0][0] == sign:
+        if board[0][1]== sign:
+            if board[0][2] == sign:
+                return True
+        if board[1][0] == sign:
+            if board[2][0] == sign:
+                return True
+        if board[1][1] == sign:
+            if board[2][2] == sign:
+                return True
+    if board[2][2] == sign:
+        if board[2][1] == sign:
+            if board[2][0] == sign:
+                return True
+        if board[1][2] == sign:
+            if board[0][2]== sign:
+                return True
+    if board[1][1] == sign:
+        if board[1][0] == sign:
+            if board[1][2] == sign:
+                return True
+        if board[0][1] == sign:
+            if board[2][1] == sign:
+                return True
+        if board[0][2] == sign:
+            if board[2][0] == sign:
+                return True
+    return False
+
+
+
+
+def play(choice_board,index):
    
     while True :
+        board = -1*np.ones((3,3),dtype='int32')
         toss_input = int(input('choose Head(0) or Tail(1)'))
         toss = np.random.choice([0,1])
         if toss_input == toss:
             print('congrats you won the toss ! \n \n your sign is 0')
             while True:
-                if not np.any((board==-1)):
-                    print('zook')
+                if  np.all((board!=-1)):
+                    
                     break
                
                 while True:
@@ -47,6 +90,9 @@ def play(board,weights,choice_board,index):
                     print('Choose again retard ')
                 board = choose(board,choice_pos,0)
                 print_(board)
+                if check_result(board,0):
+                    print("yay! you won")
+                    break
                 try:
                     random_pos = choice_board[np.random.choice(index[(board==-1).flatten()])]
                 except:
@@ -55,6 +101,9 @@ def play(board,weights,choice_board,index):
                     break
                 board = choose(board,random_pos,1)
                 print_(board)
+                if check_result(board,1):
+                    print("you lose")
+                    break
         else:
             print('opps ! loss the toss \n \n your sign is 1')
 
@@ -68,7 +117,11 @@ def play(board,weights,choice_board,index):
                     break
                 board = choose(board,random_pos,0)
                 print_(board)
-                
+                if check_result(board,0):
+                    print("you lose ")
+                    break
+                if np.all((board!=-1)):
+                    break
                 while True:
                     choice_pos = int(input('Choose a position on board (0-8)'))
                     if choice_pos in index[(board==-1).flatten()]:
@@ -78,12 +131,13 @@ def play(board,weights,choice_board,index):
                 board = choose(board,choice_pos,1)
                 
                 print_(board)
-                if not np.any((board==-1)):
-                    print('zook')
+                if check_result(board,1):
+                    print("yay! you won")
                     break
+                
         
         play_again = int(input('wanna play again'))
         if not play_again:
             break
-play(board,weights,choice_board,index)
+play(choice_board,index)
             
